777a35060a7d5b01c22be70f9ef8c5e0ffcb7c08
Matthew John <matthew@dockstudios.co.uk>
2022-07-28 06:19:19 +0100

    Fix creation of engine, removing non-sqlite compatible options, and session, using correct object
    Issue #5

--- terrarun/database.py
+++ terrarun/database.py
@@ -13,10 +13,10 @@ class Database:
     def get_engine(cls):
         """Get singleton instance of engine."""
         if cls._ENGINE is None:
-            cls._ENGINE = sqlalchemy.create_engine('sqlite:///test.db', pool_size=5, pool_recycle=3600)
+            cls._ENGINE = sqlalchemy.create_engine('sqlite:///test.db')
         return cls._ENGINE
 
     @classmethod
     def get_session(cls):
         """Return database session"""
-        return sqlalchemy.orm.session(cls.get_engine(), future=True)
+        return sqlalchemy.orm.Session(cls.get_engine(), future=True)
