2d8702131a46825f2d233c00720a490593a634a6
Matthew John <matthew@dockstudios.co.uk>
2022-07-30 13:14:05 +0100

    Add new home component
    Issue #11

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
