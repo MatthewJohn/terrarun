0cda67b9b8c67b7988faf4d0434227b1ebd87383
Matthew John <matthew@dockstudios.co.uk>
2022-07-30 13:24:31 +0100

    Add home component and sidebar
    Issue #11

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NbMenuItem, NbSidebarService } from '@nebular/theme';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ui';
+
+  constructor(private readonly sidebarService: NbSidebarService) {
+  }
+
+  toggleSidebar(): boolean {
+    this.sidebarService.toggle();
+    return false;
+  }
+  items: NbMenuItem[] = [
+    {
+      title: 'Home',
+      link: '/'
+    }
+   ];
 }
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbIconModule, NbLayoutModule, NbMenuModule, NbSidebarModule, NbThemeModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { HomeComponent } from './home/home.component';
 
@@ -17,6 +17,9 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    NbIconModule,
+    NbSidebarModule.forRoot(),
+    NbMenuModule.forRoot(),
     NbThemeModule.forRoot({ name: 'default' }),
     NbLayoutModule,
     NbEvaIconsModule
