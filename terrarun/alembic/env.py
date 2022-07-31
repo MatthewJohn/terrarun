6f96af1551e90f6dbf6516c7b6815d9246055205
Matthew John <matthew@dockstudios.co.uk>
2022-07-31 10:50:01 +0100

    Add missing migration for user token table
    Issue #11

--- terrarun/alembic/env.py
+++ terrarun/alembic/env.py
@@ -25,6 +25,7 @@ from terrarun.plan import Plan
 from terrarun.apply import Apply
 from terrarun.run_queue import RunQueue
 from terrarun.user import User
+from terrarun.user_token import UserToken
 
 from terrarun.database import Base
 target_metadata = Base.metadata
