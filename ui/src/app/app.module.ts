10477cf980890ba4138769e45e88f8b5272f53f6
Matthew John <matthew@dockstudios.co.uk>
2022-07-31 13:23:57 +0100

    Use nebular modules for login page
    Issue #11

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbIconModule, NbLayoutModule, NbMenuModule, NbSidebarModule, NbThemeModule } from '@nebular/theme';
+import { NbButtonModule, NbCardModule, NbFormFieldModule, NbIconModule, NbInputModule, NbLayoutModule, NbMenuModule, NbSidebarModule, NbThemeModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { HomeComponent } from './home/home.component';
 import { SettingsModule } from './settings/settings.module';
@@ -25,12 +25,18 @@ import { AppRedirectComponent } from './app-redirect/app-redirect.component';
     AppRoutingModule,
     SettingsModule,
     BrowserAnimationsModule,
+  
     NbIconModule,
     NbSidebarModule.forRoot(),
     NbMenuModule.forRoot(),
     NbThemeModule.forRoot({ name: 'default' }),
     NbLayoutModule,
     NbEvaIconsModule,
+    NbInputModule,
+    NbCardModule,
+    NbButtonModule,
+    NbFormFieldModule,
+
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule
--- ui/src/app/settings/settings-routing.module.ts
+++ ui/src/app/settings/settings-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoggedInGuard } from '../logged-in.guard';
 import { SettingsComponent } from './settings/settings.component';
 import { TokensComponent } from './tokens/tokens.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: SettingsComponent
+    component: SettingsComponent,
+    canActivate: [LoggedInGuard]
   },
   {
     path: 'tokens',
-    component: TokensComponent
+    component: TokensComponent,
+    canActivate: [LoggedInGuard]
   }
 ];
 
