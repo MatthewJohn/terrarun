c244017b40043ae4b811a5776d449963c84e53b3
Matthew John <matthew@dockstudios.co.uk>
2022-08-02 06:33:06 +0100

    Add authentication header to organisation name validation service method
    Issue #8

--- ui/src/app/organisation.service.ts
+++ ui/src/app/organisation.service.ts
@@ -1,18 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrganisationService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private accountService: AccountService) { }
 
   validateNewOrganisationName(name: string): Promise<any> {
     return new Promise((resolve, reject) => {
       this.http.post<any>(
         `https://${window.location.hostname}:5000/api/terrarun/v1/organisation/create/name-validation`,
-        { 'name': name }
+        { 'name': name },
+        { headers: this.accountService.getAuthHeader() }
       ).subscribe({
         next: (data) => {
           resolve(data.data);
