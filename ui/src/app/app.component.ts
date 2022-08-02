2a1447e8965ae12bb4115410fe8804a21d398773
Matthew John <matthew@dockstudios.co.uk>
2022-08-02 23:23:12 +0100

    Add example dynamic sidebar menu
    Issue #9

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NbMenuItem, NbSidebarService } from '@nebular/theme';
+import { AccountService } from './account.service';
 import { HomeComponent } from './home/home.component';
 
 @Component({
@@ -10,43 +11,54 @@ import { HomeComponent } from './home/home.component';
 export class AppComponent {
   title = 'ui';
 
-  constructor(private readonly sidebarService: NbSidebarService) {
+  constructor(private readonly sidebarService: NbSidebarService,
+              private accountService: AccountService) {
+    this.accountService.isLoggedIn().then((loggedIn) => {
+      if (loggedIn) {
+        this.items = [
+          {
+            title: 'Home',
+            link: '/home',
+            icon: 'home-outline'
+          },
+          {
+            title: 'Organisations',
+            icon: 'globe-outline',
+            children: [
+              {
+                title: 'Create',
+                link: '/organisation/create'
+              },
+              {
+                title: 'List',
+                link: '/organisation/list'
+              }
+            ]
+          },
+          {
+            title: 'Settings',
+            link: '/settings',
+            icon: 'settings-2-outline',
+            children: [
+              {
+                title: 'User Tokens',
+                link: '/settings/tokens'
+              }
+            ]
+          }
+        ];
+      } else {
+        this.items = [{
+          title: 'Login',
+          link: '/login'
+        }]
+      }
+    });
   }
 
   toggleSidebar(): boolean {
     this.sidebarService.toggle();
     return false;
   }
-  items: NbMenuItem[] = [
-    {
-      title: 'Home',
-      link: '/home',
-      icon: 'home-outline'
-    },
-    {
-      title: 'Organisations',
-      icon: 'globe-outline',
-      children: [
-        {
-          title: 'Create',
-          link: '/organisation/create'
-        },
-        {
-          title: 'List',
-          link: '/organisation/list'
-        }
-      ]
-    },
-    {
-      title: 'Settings',
-      link: '/settings',
-      icon: 'settings-2-outline',
-      children: [
-        {
-          title: 'User Tokens',
-          link: '/settings/tokens'
-        }
-      ]
-    }
-   ];
+  items: NbMenuItem[] = [];
 }
