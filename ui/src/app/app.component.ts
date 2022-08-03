df67800eec6960cd4a77f7881d4e70d22afe6912
Matthew John <matthew@dockstudios.co.uk>
2022-08-03 20:16:24 +0100

    Update to use observers to listen for authentication and current organisation state changes and reload sidebar menu
    Issue #9

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NbMenuItem, NbSidebarService } from '@nebular/theme';
 import { AccountService } from './account.service';
 import { HomeComponent } from './home/home.component';
-import { StateService } from './state.service';
+import { AuthenticationStateType, OrganisationStateType, StateService } from './state.service';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +10,11 @@ import { StateService } from './state.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'ui';
+  _authenticationState: AuthenticationStateType = {authenticated: false, id: null, username: null};
+  _currentOrganisationState: OrganisationStateType = {id: null, name: null};
+  items: NbMenuItem[] = [];
 
   constructor(private readonly sidebarService: NbSidebarService,
               private accountService: AccountService,
@@ -18,51 +22,77 @@ export class AppComponent {
     this.accountService.getAccountDetails();
 
     this.stateService.authenticationState.subscribe((data) => {
-      if (data.authenticated == true) {
-        this.items = [
-          {
-            title: 'Home',
-            link: '/home',
-            icon: 'home-outline'
-          },
-          {
-            title: 'Organisations',
-            icon: 'globe-outline',
-            children: [
-              {
-                title: 'Create',
-                link: '/organisation/create'
-              },
-              {
-                title: 'List',
-                link: '/organisation/list'
-              }
-            ]
-          },
-          {
-            title: 'Settings',
-            link: '/settings',
-            icon: 'settings-2-outline',
-            children: [
-              {
-                title: 'User Tokens',
-                link: '/settings/tokens'
-              }
-            ]
-          }
-        ];
-      } else {
-        this.items = [{
-          title: 'Login',
-          link: '/login'
-        }]
-      }
+      this._authenticationState = data;
+      this.updateMenuItems();
     });
+    this.stateService.currentOrganisation.subscribe((data) => {
+      this._currentOrganisationState = data;
+      this.updateMenuItems();
+    });
+  }
+
+  updateMenuItems() {
+    if (this._authenticationState.authenticated == true) {
+      this.items = [
+        {
+          title: 'Home',
+          link: '/home',
+          icon: 'home-outline'
+        },
+        {
+          title: 'Organisations',
+          icon: 'globe-outline',
+          children: [
+            {
+              title: 'Create',
+              link: '/organisation/create'
+            },
+            {
+              title: 'List',
+              link: '/organisation/list'
+            }
+          ]
+        },
+        {
+          title: 'Settings',
+          link: '/settings',
+          icon: 'settings-2-outline',
+          children: [
+            {
+              title: 'User Tokens',
+              link: '/settings/tokens'
+            }
+          ]
+        }
+      ];
+
+      // Add links for current organisation
+      if (this._currentOrganisationState.id) {
+        this.items[1].children?.push({
+          title: this._currentOrganisationState.name || '',
+          link: `/${this._currentOrganisationState.id}`,
+          children: [
+            {
+              title: 'Overview',
+              link: `/${this._currentOrganisationState.id}`
+            },
+            {
+              title: 'Workspaces',
+              link: `/${this._currentOrganisationState.id}/workspaces`
+            }
+          ]
+        })
+      }
+    } else {
+      this.items = [{
+        title: 'Login',
+        link: '/login'
+      }]
+    }   
   }
 
   toggleSidebar(): boolean {
     this.sidebarService.toggle();
     return false;
   }
-  items: NbMenuItem[] = [];
 }
