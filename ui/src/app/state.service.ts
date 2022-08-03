329961b4619c292e81be9aef2ba6ee7601875813
Matthew John <matthew@dockstudios.co.uk>
2022-08-03 18:34:39 +0100

    Add state for current organisation
    Issue #9

--- ui/src/app/state.service.ts
+++ ui/src/app/state.service.ts
@@ -10,19 +10,25 @@ export interface AuthenticationStateType {
     username: string | null;
 };
 
+export interface OrganisationStateType {
+    id: string | null;
+    name: string | null;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class StateService {
 
     authenticationState: BehaviorSubject<AuthenticationStateType> = new BehaviorSubject<AuthenticationStateType>({authenticated: null, id: null, username: null});
+    currentOrganisation: BehaviorSubject<OrganisationStateType> = new BehaviorSubject<OrganisationStateType>({id: null, name: null});
 
     constructor() {
         console.log("Created state object");
         this.authenticationState.subscribe((data) => {
             console.log("Authentication state updated...");
             console.log(data);
-        })
+        });
     }
 
     getAuthenticationObserver(): Observer<any> {      
