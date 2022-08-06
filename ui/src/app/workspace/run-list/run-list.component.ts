29a4efa6eb036a7a83207f5b1b7db61a715b2c3e
Matthew John <matthew@dockstudios.co.uk>
2022-08-06 09:49:33 +0100

    Attempt to show run status icon in separate column
    Issue #22

--- ui/src/app/workspace/run-list/run-list.component.ts
+++ ui/src/app/workspace/run-list/run-list.component.ts
@@ -14,7 +14,7 @@ import { WorkspaceService } from 'src/app/workspace.service';
 export class RunListComponent implements OnInit {
 
   runs$: Observable<any> | null = null;
-  tableColumns: string[] = ['id', 'runStatus', 'created-at'];
+  tableColumns: string[] = ['icon', 'id', 'runStatus', 'created-at'];
   workspaceName: string | null = null;
   organisationName: string | null = null;
 
