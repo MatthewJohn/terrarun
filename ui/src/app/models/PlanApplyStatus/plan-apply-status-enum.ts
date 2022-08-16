c5b0f664d81f8047ba6fe542941b45047bdb604e
Matthew John <matthew@dockstudios.co.uk>
2022-08-16 07:56:21 +0100

    Add coloring to plan/apply.
    Create enum/factory for plan/apply statuses
    
    Issue #24

---
+++ ui/src/app/models/PlanApplyStatus/plan-apply-status-enum.ts
@@ -0,0 +1,10 @@
+export enum PlanApplyStatusEnum {
+    PENDING = 'pending',
+    MANAGED_QUEUED = 'managed_queued',
+    QUEUED = 'queued',
+    RUNNING = 'running',
+    ERRORED = 'errored',
+    CANCELED = 'canceled',
+    FINISHED = 'finished',
+    UNREACHABLE = 'unreachable'
+}
