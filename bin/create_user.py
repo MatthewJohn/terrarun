c7533324ba3feb035bc636384cb577a1a02693ff
Matthew John <matthew@dockstudios.co.uk>
2022-09-21 12:08:01 +0100

    Add create user script

---
+++ bin/create_user.py
@@ -0,0 +1,25 @@
+#!python
+
+"""Create user"""
+
+import sys
+sys.path.append('.')
+
+from argparse import ArgumentParser
+
+import terrarun
+from terrarun.database import Database
+
+parser = ArgumentParser()
+parser.add_argument('--username', type=str, required=True)
+parser.add_argument('--password', type=str, required=True)
+parser.add_argument('--email', type=str, required=True)
+parser.add_argument('--site-admin', action='store_true')
+
+args = parser.parse_args()
+
+user = terrarun.User(username=args.username, email=args.email, site_admin=args.site_admin)
+user.password = args.password
+session = Database.get_session()
+session.add(user)
+session.commit()
