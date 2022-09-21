a6cc9c3837c54b5ef92be37dea0be64301bca583
Matthew John <matthew@dockstudios.co.uk>
2022-09-21 10:52:58 +0100

    Attempt to move to promising to obtain task result data
    Issue #39

--- ui/src/app/models/TaskResult/task-result.ts
+++ ui/src/app/models/TaskResult/task-result.ts
@@ -5,42 +5,55 @@ export class TaskResult {
     _id: string;
     _details: Promise<any> | undefined;
     _color: string | undefined;
+    _message: string | undefined;
+    _title: string | undefined;
 
     constructor(id: string,
             private taskResultService: TaskResultService) {
         this._id = id;
         this._details = undefined;
         this._color = undefined;
+        this._message = undefined;
+        this._title = undefined;
     }
 
-    getColor(): Promise<string> {
-        return new Promise((resolve, reject) => {
-            if (this._color == null) {
-                this.getDetails().then((details) => {
-                    if (details.data?.attributes?.status) {
-                        let status = details.data.attributes.status;
-                        if (status == 'pending') {
-                            this._color = 'info';
-                        } else if (status == 'running') {
-                            this._color = 'info';
-                        } else if (status == 'passed') {
-                            this._color = 'success'
-                        } else if (status == 'failed') {
-                            this._color = 'danger';
-                        } else if (status == 'errored') {
-                            this._color = 'danger';
-                        } else if (status == 'canceled') {
-                            this._color = 'danger';
-                        } else {
-                            this._color = 'basic';
-                        }
-                        resolve(this._color);
-                    }
-                });
+    async getColor(): Promise<string> {
+        if (this._color === undefined) {
+            let details = await this.getDetails();
+            let status = details.data.attributes.status;
+            if (status == 'pending') {
+                this._color = 'info';
+            } else if (status == 'running') {
+                this._color = 'info';
+            } else if (status == 'passed') {
+                this._color = 'success'
+            } else if (status == 'failed') {
+                this._color = 'danger';
+            } else if (status == 'errored') {
+                this._color = 'danger';
+            } else if (status == 'canceled') {
+                this._color = 'danger';
             } else {
-                resolve(this._color);
+                this._color = 'basic';
             }
-        });
+        }
+        return this._color;
+    }
+
+    async getTitle(): Promise<string> {
+        if (this._title === undefined) {
+            let details = await this.getDetails();
+            this._title = details.data.id;
+        }
+        return this._title || '';
+    }
+
+    async getMessage(): Promise<string> {
+        if (this._message === undefined) {
+            let details = await this.getDetails();
+            this._message = details.data.attributes.message;
+        }
+        return this._message || '';
     }
 
     getDetails(refreshDetails: boolean=false): Promise<any> {
