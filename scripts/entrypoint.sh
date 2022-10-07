320e23c1becd603f097cd340689cb6cc71617f57
Bartlomiej Wieckowski <Bartlomiej.Wieckowski@stepstone.com>
2022-10-07 15:18:36 +0200

    TEAM-1525 - Initial commit of Bamboo Plugin

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# Check if database upgrades are to be performed
+if [[ ! -z "${TERRAFORM_VERSION}" ]]
+then
+    tfenv install $TERRAFORM_VERSION
+    tfenv use $TERRAFORM_VERSION
+fi
+
+# Check if database upgrades are to be performed
+if [ "${MIGRATE_DATABASE}" == "True" ]
+then
+    alembic upgrade head
+fi
+
+# Check whether to upgrade database and exit
+if [ "${MIGRATE_DATABASE_ONLY}" == "True" ]
+then
+    exit
+fi
+
+# Run main executable
+python -u ./terrarun.py --ssl-cert-private-key ./bin/example/ssl/dev.key --ssl-cert-public-key ./bin/example/ssl/dev.crt
