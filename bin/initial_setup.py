ecf5ec5de5897575095fc44bb6823610aff211a8
Matthew John <matthew@dockstudios.co.uk>
2023-04-30 14:32:10 +0100

    Update initial setup script to perform DB migration
    Issue #69

--- bin/initial_setup.py
+++ bin/initial_setup.py
@@ -2,6 +2,7 @@
 
 """Create initial data"""
 
+import subprocess
 import sys
 sys.path.append('.')
 
@@ -15,8 +16,16 @@ parser.add_argument('--organisation', type=str, default="default")
 parser.add_argument('--organisation-email', dest="organisation_email", type=str, required=True)
 parser.add_argument('--lifecycle', type=str, default="default")
 parser.add_argument('--environment', type=str, nargs="+", default=["dev", "prod"])
+parser.add_argument('--migrate-database', action='store_true', dest='migrate_database')
 
 args = parser.parse_args()
+
+if args.migrate_database:
+    res = subprocess.check_call(['alembic', 'upgrade', 'head'])
+    if res:
+        print('Failure during DB migration')
+        sys.exit(1)
+
 
 if not (org := terrarun.Organisation.get_by_name_id(name_id=args.organisation)):
     org = terrarun.Organisation.create(name=args.organisation, email=args.organisation_email)
