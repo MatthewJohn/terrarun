42a74029b3c5675d7062dd0b24cd493299725c2c
Matthew John <matthew@dockstudios.co.uk>
2023-05-01 08:36:44 +0100

    Add job type to job queue table
    Issue #69

--- terrarun/models/run_queue.py
+++ terrarun/models/run_queue.py
@@ -2,11 +2,17 @@
 # Unauthorized copying of this file, via any medium is strictly prohibited
 # Proprietary and confidential
 
+from enum import Enum
 import sqlalchemy
 import sqlalchemy.orm
 
 from terrarun.models.base_object import BaseObject
 from terrarun.database import Base, Database
+
+
+class RunQueueType(Enum):
+    WORKER = "WORKER"
+    AGENT = "AGENT"
 
 
 class RunQueue(Base, BaseObject):
@@ -19,3 +25,11 @@ class RunQueue(Base, BaseObject):
 
     run_id = sqlalchemy.Column(sqlalchemy.ForeignKey("run.id"), nullable=False)
     run = sqlalchemy.orm.relation("Run", back_populates="run_queue")
+
+    job_type = sqlalchemy.Column(sqlalchemy.Enum(RunQueueType))
+
+    @classmethod
+    def get_job_by_type(cls, type_):
+        """Get first run by type"""
+        session = Database.get_session()
+        return session.query(cls).filter(cls.job_type==type_).first()
