e7ebf3c4cc2c328f2671ec4ba0edf221998a36b1
Matthew John <matthew@dockstudios.co.uk>
2023-05-13 18:26:41 +0100

    feat(db): Add API ID to lifecycle enivornment so that it can accessed via the API
    Issue #60

--- terrarun/models/lifecycle_environment.py
+++ terrarun/models/lifecycle_environment.py
@@ -6,18 +6,30 @@ import sqlalchemy
 import sqlalchemy.orm
 
 from terrarun.database import Base
+from terrarun.models.base_object import BaseObject
 import terrarun.models.environment
 import terrarun.models.lifecycle
 
 
-class LifecycleEnvironment(Base):
+class LifecycleEnvironment(Base, BaseObject):
     """Define lifecycle environment."""
 
     __tablename__ = "lifecycle_environment"
 
-    lifecycle_id = sqlalchemy.Column(sqlalchemy.ForeignKey("lifecycle.id"), primary_key=True)
-    environment_id = sqlalchemy.Column(sqlalchemy.ForeignKey("environment.id"), primary_key=True)
+    ID_PREFIX = 'lce'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    api_id_fk = sqlalchemy.Column(sqlalchemy.ForeignKey("api_id.id", name="fk_lifecycle_environment_api_id_id"), nullable=True)
+    api_id_obj = sqlalchemy.orm.relation("ApiId", foreign_keys=[api_id_fk])
+
+    lifecycle_id = sqlalchemy.Column(sqlalchemy.ForeignKey("lifecycle.id", name="fk_lifecycle_environment_lifecycle_id_lifecycle_id"))
+    environment_id = sqlalchemy.Column(sqlalchemy.ForeignKey("environment.id", name="fk_lifecycle_environment_environment_id_environment_id"))
     order = sqlalchemy.Column(sqlalchemy.Integer)
+
+    __table_args__ = (
+        sqlalchemy.UniqueConstraint('lifecycle_id', 'environment_id', name='_lifecycel_id_environment_id_uc'),
+    )
+
 
     @property
     def environment(self):
