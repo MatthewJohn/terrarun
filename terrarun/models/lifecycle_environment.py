bbbd85500e283600a413dfdca68b175b75ea061b
Matthew John <matthew@dockstudios.co.uk>
2023-05-14 15:30:43 +0100

    fix(db): Fix database migrations for lifecycle environments table, handling old primary keys and deleting foreign keys
    Issue #60

--- terrarun/models/lifecycle_environment.py
+++ terrarun/models/lifecycle_environment.py
@@ -24,15 +24,17 @@ class LifecycleEnvironment(Base, BaseObject):
 
     lifecycle_environment_group_id = sqlalchemy.Column(sqlalchemy.ForeignKey(
         "lifecycle_environment_group.id",
-        name="fk_lifecycle_environment_lifecycle_environment_group_id"))
+        name="fk_lifecycle_environment_lifecycle_environment_group_id"),
+        nullable=False)
     lifecycle_environment_group = sqlalchemy.orm.relationship("LifecycleEnvironmentGroup", back_populates="lifecycle_environments")
-    environment_id = sqlalchemy.Column(sqlalchemy.ForeignKey("environment.id", name="fk_lifecycle_environment_environment_id_environment_id"))
+    environment_id = sqlalchemy.Column(sqlalchemy.ForeignKey(
+        "environment.id", name="fk_lifecycle_environment_environment_id_environment_id"), nullable=False)
     environment = sqlalchemy.orm.relationship("Environment", back_populates="lifecycle_environments")
 
     order = sqlalchemy.Column(sqlalchemy.Integer)
 
     __table_args__ = (
-        sqlalchemy.UniqueConstraint('lifecycle_environment_group_id', 'environment_id', name='_lifecycel_id_environment_id_uc'),
+        sqlalchemy.UniqueConstraint('lifecycle_environment_group_id', 'environment_id', name='_lifecycle_environment_group_id_environment_id_uc'),
     )
 
     def get_api_details(self):
