328a03470dd00b9dd753ff6e3b13bab19fdfcba6
Matthew John <matthew@dockstudios.co.uk>
2023-07-20 06:44:33 +0100

    Move unlocking of workspace when cancelled a run to agent/worker code for updating the instance. This means a workspace isn't unlocked whilst terraform is running in the background
    Issue #132

--- terrarun/models/run.py
+++ terrarun/models/run.py
@@ -11,7 +11,7 @@ import sqlalchemy
 import sqlalchemy.orm
 import sqlalchemy.sql
 from terrarun.api_request import ApiRequest
-from terrarun.errors import FailedToUnlockWorkspaceError, RunCannotBeDiscardedError
+from terrarun.errors import FailedToUnlockWorkspaceError, RunCannotBeCancelledError, RunCannotBeDiscardedError
 from terrarun.models.audit_event import AuditEvent, AuditEventType
 
 from terrarun.database import Base, Database
@@ -194,12 +194,9 @@ class Run(Base, BaseObject):
 
     def cancel(self, user):
         """Cancel run"""
-        # @TODO Check status
-        # @TODO Should the workspace be immediately unlocked or once
-        # the job status has checked in and the run has _actually_ stopped?
+        if self.status in [RunStatus.APPLIED, RunStatus.PLANNED_AND_FINISHED, RunStatus.POST_PLAN_COMPLETED]:
+            raise RunCannotBeCancelledError("Run is not in a state that can be cancelled")
         self.update_status(RunStatus.CANCELED, current_user=user)
-        if not self.configuration_version.workspace.unlock(run=self):
-            raise FailedToUnlockWorkspaceError("Failed to unlock run")
 
     def discard(self, user):
         """Discard run"""
