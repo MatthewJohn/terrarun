ac74e0405fdf58ab42b1bc3e2ab3c2f5619e4f52
Matthew John <matthew@dockstudios.co.uk>
2024-08-06 05:43:02 +0100

    chore: Remove worker container and unused functions
    Issue #203

--- terrarun/terraform_command.py
+++ terrarun/terraform_command.py
@@ -38,19 +38,6 @@ class TerraformCommandState(Enum):
 
 class TerraformCommand(BaseObject):
 
-    STATE_FILE = 'terraform.tfstate'
-    PLAN_OUTPUT_FILE = 'TFRUN_PLAN_OUT'
-
-    def _pull_latest_state(self, work_dir):
-        """Pull latest version of state to working copy."""
-        # No latest state available for workspace
-        state_version = self.run.configuration_version.workspace.latest_state
-        if not state_version or not state_version.state_json:
-            return
-
-        with open(os.path.join(work_dir, self.STATE_FILE), 'w') as state_fh:
-            state_fh.write(json.dumps(state_version.state_json))
-
     def append_output(self, data, no_append=False):
         """Append to output"""
         session = Database.get_session()
@@ -68,70 +55,6 @@ class TerraformCommand(BaseObject):
             log.data += data
         session.add(log)
         session.commit()
-
-    def execute(self):
-        raise NotImplementedError
-
-    @classmethod
-    def _command_cancel_check(cls, obj_id, command_proc):
-        """Check for command cancel"""
-        session = sqlalchemy.orm.Session(Database.get_engine())
-        obj = session.query(cls).filter(cls.id==obj_id).first()
-        while command_proc.poll() is None:
-            session.refresh(obj)
-            if obj.run.status == terrarun.models.run.RunStatus.CANCELED:
-                command_proc.kill()
-                break
-            sleep(0.5)
-        session.close()
-
-    def _run_command(self, command, work_dir, environment_variables=None):
-        """Run system command, pping output into output object"""
-
-        if environment_variables is None:
-            environment_variables = os.environ.copy()
-
-        try:
-            logger.info('Running command: %s', command)
-            command_proc = subprocess.Popen(
-                command,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT,
-                cwd=work_dir,
-                env=environment_variables)
-        except:
-            self.append_output(b'An error occured whilst starting the run')
-            return 255
-
-        cancel_monitor = threading.Thread(
-            target=self.__class__._command_cancel_check,
-            args=(self.id, command_proc,))
-        cancel_monitor.start()
-
-        # Obtain all stdout
-        print_lines = True
-        while True:
-            line = command_proc.stdout.readline()
-            if line:
-                # Stop printing lines once line about
-                # saving plan to output file is displayed and
-                # display line about how to apply changes
-                if line.startswith(b'Saved the plan to: '):
-                    self.append_output(b'To perform exactly these actions, run the following command to apply:\n    terraform apply\n')
-                    print_lines = False
-
-                if print_lines:
-                    self.append_output(line)
-            elif command_proc.poll() is not None:
-                break
-
-        return command_proc.returncode
-
-    def add_output(self, output):
-        """Record command output."""
-        with Database.get_session() as session:
-            self.output += output
-            session.commit()
 
     def update_status(self, new_status, session=None):
         """Update state of plan."""
@@ -166,22 +89,4 @@ class TerraformCommand(BaseObject):
                 terrarun.models.audit_event.AuditEvent.object_type==self.ID_PREFIX,
                 terrarun.models.audit_event.AuditEvent.event_type==terrarun.models.audit_event.AuditEventType.STATUS_CHANGE
             )
-        }
-
-    @property
-    def status_timestamps(self):
-        """Return dict of status timestamps for API"""
-
-        session = Database.get_session()
-        def convert_event_name(event):
-            name = Database.decode_blob(event)
-            if name == TerraformCommandState.RUNNING:
-                name = 'started'
-            return '{}-at'.format(name.replace('_', '-'))
-        return {
-            convert_event_name(event.new_value): terrarun.utils.datetime_to_json(event.timestamp)
-            for event in session.query(terrarun.models.audit_event.AuditEvent).where(
-                terrarun.models.audit_event.AuditEvent.object_id==self.id,
-                terrarun.models.audit_event.AuditEvent.object_type==self.ID_PREFIX,
-                terrarun.models.audit_event.AuditEvent.event_type==terrarun.models.audit_event.AuditEventType.STATUS_CHANGE)
         }
